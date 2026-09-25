65139188b9a082fa3e015da4c55dd3d6aacd3e5b
Jesus Miranda <jesus.miranda324.biz@gmail.com>
2026-09-24 20:59:37 -0500

    created SlayTheSpire.java and added notes

---
+++ SlayTheSpire.java
@@ -0,0 +1,31 @@
+public class SlayTheSpire {
+    /*
+    *  (5) The software needs to read the deck from a plain text file in which each row
+contains a single card name and its cost. The input file will follow the following
+format: <card name>:<cost>
+    *
+    * The software must generate a unique random 9-digit id for each deck
+1) The system should produce a report containing the following:
+• (2) Deck id,
+• (2) Total cost of the cards in the Deck, and a
+• (5) Histogram of the cards in the deck.
+
+   ) The software must name the report files using the format SpireDeck .pdf (E.g,
+SpireDeck 252644859.pdf)
+5. (2) The software should handle from 0 up to 1000 cards per deck.
+6. (4) If an invalid card is read (i.e., a card with an invalid cost value, a card with an
+empty name, a card with a name that is only spaces or tabs) in the input file, the
+system will ignore that card. And include a list of invalid cards in the report.
+7. (3) If more than 10 invalid cards are read in the input file or the input file has more
+than 1000 cards, the system will produce a void report. The void report will contain
+the message VOID instead of the information that would appear on a regular
+report. The name of a void report will follow the format SpireDeck id(VOID).pdf
+(E.g, SpireDeck 252644859(VOID).pdf)
+*
+* 1) The costs should be expressed in energy (e.g., 1 energy, 3.5 energy).
+5
+10. Every method in the program must contain documentation outlining, input, out-
+put, and an overview of the steps the method executes.
+* */
+
+}
